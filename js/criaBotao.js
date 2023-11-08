fb411952b679f167b5e8e8b51dacb9fe3eef4ee4
DaviRSouza <144457305+DaviRSouza@users.noreply.github.com>
2023-11-08 02:01:08 -0300

    100%

--- js/criaBotao.js
+++ js/criaBotao.js
@@ -6,6 +6,24 @@ botoesInfo.forEach(info => {
     const botao = document.createElement('button');
     botao.textContent = info.nome;
 
+    botao.style.border = '1px solid #fff';
+    botao.style.borderRadius = '50px';
+    botao.style.padding = '10px 20px';
+    botao.style.margin = '5px'
+    botao.style.backgroundColor = 'transparent';
+    botao.style.color = '#fff';
+    botao.style.cursor = 'pointer';
+    botao.addEventListener('mouseenter', () => {
+        botao.style.transition = 'background-color 0.5s ease';
+        botao.style.backgroundColor = '#1f2549';
+        botao.style.border = '1px solid #b38828';
+
+    });
+    botao.addEventListener('mouseleave', () => {
+        botao.style.border = '1px solid #fff';
+        botao.style.backgroundColor = 'transparent';
+    });
+
     botao.addEventListener('click', () => {
         info.funcao();
     });
